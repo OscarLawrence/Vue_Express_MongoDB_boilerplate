e3e5b79ba5872f568bf9daff06404199340a9cc3
Vindao <vindao@outlook.com>
2019-11-18 16:46:45 +0100

    changes

--- config/server/secrets/keys.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  MongoURI:
-    "mongodb://dev:m-gBA]R2O$O)@ds054118.mlab.com:54118/secretsanta2019",
-  SecretKey: "dickeMilchDruese",
-  sendinBlue_Api:
-    "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"
-};
